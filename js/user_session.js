8b0a18a6c834a73f672b2e725acac319128587ef
khush-jetzy <68341564+khush-jetzy@users.noreply.github.com>
2020-07-15 12:35:28 -0400

    Update user_session.js

--- js/user_session.js
+++ js/user_session.js
@@ -1,4 +1,4 @@
-let api = "http://jetzyapi.herokuapp.com/UserSession"; //Beware of Cors Error due to running on localhost.
+let api = "https://jetzyapi.herokuapp.com/UserSession"; //Beware of Cors Error due to running on localhost.
 fetch(api)
   .then(response =>{
     return response.json();
