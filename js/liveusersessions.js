5d255e4460f40535efbd7752ed34f80300bdbace
Khush Tated <khushtated@gmail.com>
2020-07-20 20:19:45 -0400

    Number Animation fix

--- js/liveusersessions.js
+++ js/liveusersessions.js
@@ -13,7 +13,7 @@ fetch(weeklyAPI)
   .then(data => {
    liveWeeklyCount = data["StatusSummary"]; 
    var number = parseInt(liveWeeklyCount); 
-   liveWeekly.innerHTML = liveWeeklyCount; 
+    animateValue("liveWeekly", 0, number, 1000); 
 
 })
 
@@ -25,7 +25,9 @@ fetch(monthlyAPI)
   })
 
   .then(data => {
-   liveMonthly.innerHTML = data["StatusSummary"]; 
+    liveMonthlyCount = data["StatusSummary"]; 
+   var number = parseInt(liveMonthlyCount);
+   animateValue("liveMonthly", 0, number, 2000);
 })
 
 let yearlyAPI = "https://jetzyapi.herokuapp.com/liveUserSession/365"; //Beware of Cors Error due to running on localhost.
@@ -36,5 +38,39 @@ fetch(yearlyAPI)
   })
 
   .then(data => {
-   liveYearly.innerHTML = data["StatusSummary"]; 
+    liveYearlyCount = data["StatusSummary"]; 
+   var number = parseInt(liveYearlyCount);
+   animateValue("liveYearly", 0, number, 3000);
 })
+
+function animateValue(id, start, end, duration) {
+    // assumes integer values for start and end
+    
+    var obj = document.getElementById(id);
+    var range = end - start;
+    // no timer shorter than 50ms (not really visible any way)
+    var minTimer = 50;
+    // calc step time to show all interediate values
+    var stepTime = Math.abs(Math.floor(duration / range));
+    
+    // never go below minTimer
+    stepTime = Math.max(stepTime, minTimer);
+    
+    // get current time and calculate desired end time
+    var startTime = new Date().getTime();
+    var endTime = startTime + duration;
+    var timer;
+  
+    function run() {
+        var now = new Date().getTime();
+        var remaining = Math.max((endTime - now) / duration, 0);
+        var value = Math.round(end - (remaining * range));
+        obj.innerHTML = value;
+        if (value == end) {
+            clearInterval(timer);
+        }
+    }
+    
+    timer = setInterval(run, stepTime);
+    run();
+}
